218e5afcc5b28184140b527ead246c47b30e4b91
Victor Pereira <dvictorps@gmail.com>
2026-02-02 15:48:06 -0300

    refactor: Replace GameClass structure with a simplified CLASSES array and remove unused item modifier files

---
+++ src/gameData/items/items.ts
@@ -0,0 +1,38 @@
+export const ITEM_CATEGORIES = [
+    {
+        id: "weapon",
+        name: "Weapon",
+        description: "A tool used to attack enemies.",
+    },
+    {
+        id: "armor",
+        name: "Armor",
+        description: "A piece of clothing that protects the player from damage.",
+    },
+    {
+        id: "jewelry",
+        name: "Jewelry",
+        description: "A piece of jewelry that can be worn on the player.",
+    },
+    {
+        id: "consumable",
+        name: "Consumable",
+        description: "A consumable item that can be used by the player.",
+    },
+    {
+        id: "material",
+        name: "Material",
+        description: "A material that can be used to craft items.",
+    },
+    {
+        id: "quest",
+        name: "Quest",
+        description: "A quest item that can be used to complete a quest.",
+    },
+    {
+        id: "magic",
+        name: "Magic",
+        description: "A magic item that can be used to cast spells.",
+    }
+]
+
